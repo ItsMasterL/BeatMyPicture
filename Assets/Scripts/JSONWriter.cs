31f871a1690b5808257bf7e81c48dad8f03ff249
Master L <kingcreeper7777@gmail.com>
2022-11-09 18:42:37 -0800

    Setup for whats to come
    Added back buttons to menus, set up template menu hopefully all the way besides the editors themselves and button functionality. Feeling good about the organization tbh

--- Assets/Scripts/JSONWriter.cs
+++ Assets/Scripts/JSONWriter.cs
@@ -8,30 +8,32 @@ public class JSONWriter : MonoBehaviour
     [System.Serializable]
     public class Frame
     {
-        public int FrameID = 0;
-        public string image = "000";
-        public float seconds = 0.5f;
-        public float movex = 0;
-        public float movey = 0;
-        public bool invincible = false;
-        public float damage = 0;
-        public float dmgxoffset = 0;
-        public float dmgyoffset = 0;
-        public float dmgradius = 0;
-        public float projectilelife = 0;
-        public float projectilevectorx = 0;
-        public float projectilevectory = 0;
-        public string projectileimage = null;
-        public bool cancancel = true;
-        public bool hasgravity = true;
-        public string sound = null;
-        public string nextset = null;
+        public string Version = "0.0.1"; //Not in editor
+        public int FrameID = 0; //Not editable
+        public string image = "000"; //Coresponds with cutout. Default to 000 if not found
+        public float seconds = 0.5f; //Default to 0.5f if <= 0
+        public float movex = 0; //pos is forward, neg is backward
+        public float movey = 0; //pos is up, neg is down
+        public bool invincible = false; //This can be abused. Try not to though.
+        public float damage = 0; //absolute value
+        public float dmgxoffset = 0; //pos is forward, neg is backward
+        public float dmgyoffset = 0; //pos is up, neg is down
+        public float dmgradius = 0; //every hitbox is circles
+        public float projectilelife = 0; //0 means no projectile
+        public float projectilevectorx = 0; //pos is forward, neg is backward
+        public float projectilevectory = 0; //pos is up, neg is down
+        public string projectileimage = null; //default to 000. Hey, I just shot myself! No, not like that, i mean, I came out of my gun
+        public bool cancancel = true; //If false, inputs are ignored while frame is active. Prevents turbo mode
+        public bool hasgravity = true; //If false, gravity is tempoararily disabled during this frame, useful for precise control of character
+        public string sound = null; //mp3 or wav. Hard length limit of probably 5 seconds, but unlimited layering unless theres an issue
+        public string nextset = null; //Intended use for frame at end of set, to loop animations or easily use shared animations (Can softlock if cancancel is false on all frames!!)
     }
 
     [System.Serializable]
     public class AnimSet
     {
         public string SetName;
+        public int SetID;
         public Frame[] frame;
     }
     
